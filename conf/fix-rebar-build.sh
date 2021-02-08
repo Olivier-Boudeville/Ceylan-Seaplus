9ba1eaae75e265533c67f463be9270652eb9b33d
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-02-08 22:06:38 +0100

    Robustified.

--- conf/fix-rebar-build.sh
+++ conf/fix-rebar-build.sh
@@ -2,12 +2,29 @@
 
 # Script defined for convenience.
 
-/bin/mv -f src/seaplus_parse_transform.erl src/seaplus_parse_transform.erl-hidden
+# May not be here if Seaplus is a checkout:
+parse_src="src/seaplus_parse_transform.erl"
+
+if [ -f "${parse_src}" ]; then
+	/bin/mv -f ${parse_src} ${parse_src}-hidden
+fi
 
 target_dir="./_build/default/lib/seaplus/ebin/"
 
 if [ ! -d "${target_dir}" ]; then
-	target_dir="../seaplus/ebin"
+	target_dir="../seaplus/ebin/"
 fi
 
-/bin/cp -f src/seaplus_parse_transform.beam "${target_dir}"
+echo "Selected target directory for Seaplus parse transform is '${target_dir}'."
+
+parse_beam="src/seaplus_parse_transform.beam"
+
+if [ -f "${parse_beam}" ]; then
+
+	/bin/cp -f ${parse_beam} ${target_dir}
+
+else
+
+	echo "Warning: no ${parse_beam} found from $(pwd); content: $(tree)"
+
+fi
